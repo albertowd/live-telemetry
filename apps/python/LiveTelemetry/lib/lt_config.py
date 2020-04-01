3f82b6a20b561452646caea72428236074831f76
Alberto Dietrich <alberto+git@albertowd.com.br>
2020-03-31 22:01:26 -0300

    Configs improvements.

--- apps/python/LiveTelemetry/lib/lt_config.py
+++ apps/python/LiveTelemetry/lib/lt_config.py
@@ -33,11 +33,24 @@ class Config(object):
 
             Config.__configs = configparser.ConfigParser()
             Config.__configs["About"] = {"Version": lt_version}
-            Config.__configs["Options"] = {
-                "Load": "True", "Logging": "False", "Size": "FHD"}
-            Config.__configs["Positions"] = {}
-            Config.__configs["Windows"] = {
-                "Engine": "False", "FL": "False", "FR": "False", "RL": "False", "RR": "False"}
+
+            self.set_option_active("Camber", True)
+            self.set_option_active("Dirt", True)
+            self.set_option_active("Height", True)
+            self.set_option_active("Load", True)
+            self.set_option_active("Logging", True)
+            self.set_option_active("Pressure", True)
+            self.set_option_active("Temps", True)
+            self.set_option_active("Size", "FHD")
+            self.set_option_active("Suspension", True)
+            self.set_option_active("Tire", True)
+            self.set_option_active("Wear", True)
+
+            self.set_window_active("EN", False)
+            self.set_window_active("FL", False)
+            self.set_window_active("FR", False)
+            self.set_window_active("RL", False)
+            self.set_window_active("RR", False)
 
             h = 720
             w = 1280
@@ -50,65 +63,40 @@ class Config(object):
             except:
                 log("Could not get 'cfg/video.ini' video options, using default 1280x720 resolution.")
 
-            self.set_engine_position((w - 360) / 2, h - 51 - 160)
-            self.set_options_position(w - 200 - 50, (h - 110) / 2)
-            self.set_position("FL", 10, 80)
-            self.set_position("FR", w - 360 - 10, 80)
-            self.set_position("RL", 10, h - 163 - 80)
-            self.set_position("RR", w - 360 - 10, h - 163 - 80)
+            self.set_window_position("EN", [(w - 360) / 2, h - 51 - 160])
+            self.set_window_position("FL", [10, 80])
+            self.set_window_position("FR", [w - 360 - 10, 80])
+            self.set_window_position("OP", [w - 200 - 50, (h - 110) / 2])
+            self.set_window_position("RL", [10, h - 163 - 80])
+            self.set_window_position("RR", [w - 360 - 10, h - 163 - 80])
             self.save_config()
 
-    def get_engine_x(self):
-        """ Returns the x position of the engine window. """
-        return float(self.get_str("Positions", "Engine_x"))
-
-    def get_engine_y(self):
-        """ Returns the y position of the engine window. """
-        return float(self.get_str("Positions", "Engine_y"))
-
-    def get_options_x(self):
-        """ Returns the x position of the options window. """
-        return float(self.get_str("Positions", "Options_x"))
-
-    def get_options_y(self):
-        """ Returns the y position of the options window. """
-        return float(self.get_str("Positions", "Options_y"))
-
-    def get_resolution(self):
-        """ Returns the windows resolution. """
-        return self.get_str("Options", "Size")
-
-    def get_str(self, section, option):
+    def __get_str(self, section, option):
         """ Returns an option. """
         return Config.__configs.get(section, option)
 
+    def __set_str(self, section, option, value):
+        """ Updates an option. """
+        Config.__configs.set(section, option, value)
+
     def get_version(self):
         """ Returns the config file version. """
-        return self.get_str("About", "version")
+        return self.__get_str("About", "version")
 
-    def get_x(self, wheel_id):
-        """ Returns the x position of window. """
-        return float(self.get_str("Positions", "{}_x".format(wheel_id)))
+    def get_window_position(self, name):
+        """ Returns the position [x,y] of a window. """
+        return [
+            float(self.__get_str("Windows Positions", "{}_x".format(name))),
+            float(self.__get_str("Windows Positions", "{}_y".format(name)))
+        ]
 
-    def get_y(self, wheel_id):
-        """ Returns the y position of window. """
-        return float(self.get_str("Positions", "{}_y".format(wheel_id)))
+    def is_option_active(self, name):
+        """ Returns if a option is active. """
+        return self.__get_str("Options", name) == "True"
 
-    def is_active(self, wheel_id):
-        """ Returns if window is active. """
-        return self.get_str("Windows", wheel_id).lower() == "true"
-
-    def is_engine_active(self):
-        """ Returns if engine window is active. """
-        return self.get_str("Windows", "Engine").lower() == "true"
-
-    def is_load_active(self):
-        """ Returns if load feature is active. """
-        return self.get_str("Options", "Load").lower() == "true"
-
-    def is_logging_active(self):
-        """ Returns if logging is active. """
-        return self.get_str("Options", "Logging").lower() == "true"
+    def is_window_active(self, name):
+        """ Returns if a window is active. """
+        return self.__get_str("Windows", name) == "True"
 
     def save_config(self):
         """ Writes the actual options on the configuration file. """
@@ -116,41 +104,17 @@ class Config(object):
         Config.__configs.write(cfg_file)
         cfg_file.close()
 
-    def set_active(self, wheel_id, active):
-        """ Updates if window is active. """
-        self.set_str("Windows", wheel_id, str(active))
+    def set_option_active(self, name, active):
+        """ Updates if a option is active. """
+        self.__set_str("Options", name, str(active))
 
-    def set_engine_active(self, active):
-        """ Updates if engine window is active. """
-        self.set_str("Windows", "Engine", str(active))
+    def set_window_active(self, name, active):
+        """ Updates if a window is active. """
+        self.__set_str("Windows", name, str(active))
 
-    def set_options_position(self, pos_x, pos_y):
-        """ Updates options window position. """
-        self.set_str("Positions", "Options_x", str(pos_x))
-        self.set_str("Positions", "Options_y", str(pos_y))
-
-    def set_engine_position(self, pos_x, pos_y):
-        """ Updates engine window position. """
-        self.set_str("Positions", "Engine_x", str(pos_x))
-        self.set_str("Positions", "Engine_y", str(pos_y))
-
-    def set_load_active(self, enabled):
-        """ Updates logging status. """
-        self.set_str("Options", "Load", str(enabled))
-
-    def set_logging_active(self, logging):
-        """ Updates logging status. """
-        self.set_str("Options", "Logging", str(logging))
-
-    def set_position(self, wheel_id, pos_x, pos_y):
-        """ Updates window position. """
-        self.set_str("Positions", "{}_x".format(wheel_id), str(pos_x))
-        self.set_str("Positions", "{}_y".format(wheel_id), str(pos_y))
-
-    def set_resolution(self, resolution):
-        """ Updates windows resolution. """
-        self.set_str("Options", "Size", str(resolution))
-
-    def set_str(self, section, option, value):
-        """ Updates an option. """
-        Config.__configs.set(section, option, value)
+    def set_window_position(self, name, position):
+        """ Updates a window position. """
+        self.__set_str("Windows Positions",
+                       "{}_x".format(name), str(position[0]))
+        self.__set_str("Windows Positions",
+                       "{}_y".format(name), str(position[1]))
